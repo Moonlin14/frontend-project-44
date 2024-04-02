d7485c14da31135a982a1e59b201bb40ce19b3d5
Данил Сидельников <danilsidelnikov74@mail.ru>
2024-04-02 15:36:11 +0300

    add eslint and fix errors

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,6 @@
-import readlineSync from 'readline-sync'
-
+import readlineSync from 'readline-sync';
 
 export default () => {
   const name = readlineSync.question('May i have your name? ');
   console.log(`Hello, ${name}!`);
 };
-
