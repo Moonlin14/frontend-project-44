ddb961e40e874f6b7458c161affd4f3506332a86
Данил Сидельников <danilsidelnikov74@mail.ru>
2024-04-07 21:14:05 +0300

    fix even-game

--- src/even-game.js
+++ src/even-game.js
@@ -1,30 +1,20 @@
-import readlineSync from 'readline-sync';
+import run from '../src/index.js';
+
+const explanation = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const randomNum = () => {
   const random = Math.round(Math.random() * 100);
   return random;
 };
 
+
+const even = () => {
+  const number = randomNum();
+  const question = number.toString();
+  const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
+
 export default () => {
-  const name = readlineSync.question('May i have your name? ');
-  console.log(`Hello, ${name}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  let question = randomNum();
-  let trueCount = 0;
-  do {
-    console.log(`Question: ${question = randomNum()}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (answer === 'yes' && question % 2 === 0) {
-      console.log('Correct!');
-      trueCount += 1;
-    } else if (answer === 'no' && question % 2 !== 0) {
-      console.log('Correct!');
-      trueCount += 1;
-    } else if (answer === 'yes' && question % 2 !== 0) {
-      return console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\n Let's try again, ${name}!`);
-    } else if (answer === 'no' && question % 2 === 0) {
-      return console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.\n Let's try again, ${name}!`);
-    }
-  } while (trueCount < 3);
-  return console.log(`Congratulations, ${name}!`);
+  run(explanation, even);
 };
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import readlineSync from 'readline-sync';
+
+export default (explanation, coll) => {
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May i have your name? ');
+    console.log(`Hello, ${name}!`);
+    console.log(explanation);
+    
+    for (let i = 0; i <= 3; i += 1) {
+        const [question, correctAnswer] = coll;
+        console.log(`Question: ${question}`);
+        const userAnswer = readlineSync.question('Your answer: ');
+        if (userAnswer !== correctAnswer) {
+        return console.log(`'${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.\n Let's try again, ${name}!`); 
+        };
+        console.log('Correct');
+    };
+
+    console.log(`Congratulations, ${name}!`);
+};
