6ce708acee13c3d6699c1bcaf2f918bb952f92d1
Данил Сидельников <danilsidelnikov74@mail.ru>
2024-04-09 18:39:19 +0300

    add brain-gsd

---
+++ bin/brain-gsd.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import gsd from '../src/games/gsd-game.js'
+
+gsd();
---
+++ src/games/gsd-game.js
@@ -0,0 +1,4 @@
+import run from '../index.js';
+
+const explanation = 'Find the greatest common divisor of given numbers.';
+
