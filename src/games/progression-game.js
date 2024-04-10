79f2262d86aac18ca3ed6d8f3f228138bee509f6
Данил Сидельников <danilsidelnikov74@mail.ru>
2024-04-10 13:22:32 +0300

    finish brain-progression

--- src/games/progression-game.js
+++ src/games/progression-game.js
@@ -6,3 +6,27 @@ const randomNum = (num) => {
   const random = Math.round(Math.random() * num);
   return random;
 };
+
+const randomColl = () => {
+  const coll = [];
+  const num1 = randomNum(20);
+  const num2 = randomNum(5);
+  const space = randomNum(10);
+  for (let i = num2; i < num2 * 11; i += num2) {
+    coll.push(num1 + i);
+  };
+  const correctAnswer = coll[space];
+  coll[space] = '..';
+  return [correctAnswer, coll];
+};
+
+const progression = () => {
+  const [answer, coll] = randomColl();
+  const correctAnswer = `${answer}`;
+  const question = `${coll}`;
+  return [question, correctAnswer];
+};
+
+export default () => {
+  run(explanation, progression);
+};
