d0c82082112ad23dae5e53b3cf2f64bcf98c0897
Данил Сидельников <danilsidelnikov74@mail.ru>
2024-04-10 20:14:19 +0300

    fix brain-games

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
 export default () => {
-  const name = readlineSync.question('May i have your name? ');
   console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May i have your name? ');
   console.log(`Hello, ${name}!`);
   return name;
 };
