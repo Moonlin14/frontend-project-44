d6b319c92fdfb480a1caeb75d0cdc87b60b2edbb
Данил Сидельников <danilsidelnikov74@mail.ru>
2024-04-11 14:46:59 +0300

    fix brain-progression

--- src/games/progression-game.js
+++ src/games/progression-game.js
@@ -2,16 +2,13 @@ import run from '../index.js';
 
 const explanation = 'What number is missing in the progression?';
 
-const randomNum = (num) => {
-  const random = Math.round(Math.random() * num);
-  return random;
-};
+const randomNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 const randomColl = () => {
   const coll = [];
-  const num1 = randomNum(30);
-  const num2 = randomNum(5);
-  const space = randomNum(9);
+  const num1 = randomNum(1, 30);
+  const num2 = randomNum(1, 5);
+  const space = randomNum(1, 10);
   for (let i = num2; i < num2 * 11; i += num2) {
     coll.push(num1 + i);
   }
